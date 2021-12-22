49999d48f7922d5fd1165b0ecf61a45d6b13c2b9
Matthias Diener <mdiener@illinois.edu>
2021-12-22 20:40:03 +0100

    lintl

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+export LDFLAGS="$LDFLAGS -lintl"
+
 ./configure --prefix=$PREFIX
 
 make -j${CPU_COUNT}
