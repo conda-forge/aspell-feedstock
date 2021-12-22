40f5c1035312d36768d1d747a7d8b3884e871791
Matthias Diener <mdiener@illinois.edu>
2021-12-22 22:28:00 +0100

    add archs (#1)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+if [[ $target_platform == "osx-arm64" ]]; then
+	export LDFLAGS="$LDFLAGS -lintl"
+fi
+
 ./configure --prefix=$PREFIX
 
 make -j${CPU_COUNT}
