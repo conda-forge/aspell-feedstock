42dbe29c211cd0725c7e424562c1e30b14187c9f
Matthias Diener <mdiener@illinois.edu>
2022-12-22 15:01:53 +0100

    add dictionaries  (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,57 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+
 if [[ $target_platform == "osx-arm64" ]]; then
-	export LDFLAGS="$LDFLAGS -lintl"
+    # For the dictionaries, it is necessary to build aspell for the build platform
+    # when cross-compiling.
+
+    # Start a subshell to not disturb the variables below
+    (
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export LD=($CC_FOR_BUILD -print-prog-name=ld)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
+    export LDFLAGS="-L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib -lintl"
+    export host_alias=$build_alias
+    export CFLAGS=""
+    export CXXFLAGS=""
+    export CPPFLAGS=""
+
+    ./configure --prefix=$PREFIX --host="$BUILD" || { cat config.log; exit 1; }
+
+    make -j${CPU_COUNT}
+
+    make install
+    )
+else
+    ./configure --prefix=$PREFIX
+
+    make -j${CPU_COUNT}
+
+    make install
 fi
 
-./configure --prefix=$PREFIX
 
-make -j${CPU_COUNT}
+# Install dictionaries
+for lang in en de es fr af am ar ast az be bg bn br ca cs csb cy da de el eo et fa fi fo fy ga gd gl grc gu gv he hi hil hr hsb hu hus hy ia id it kn ku ky la lt lv mg mi mk ml mn mr ms mt nds nl nn ny or pa pl pt_BR pt_PT qu ro ru rw sc sk sl sr sv sw ta te tet tk tl tn tr uk uz vi wa yi zu; do
+    cd $lang
+    ./configure --vars ASPELL=$PREFIX/bin/aspell PREZIP=$PREFIX/bin/prezip
+    make install
+    cd ..
+done
 
-make install
+
+if [[ $target_platform == "osx-arm64" ]]; then
+    # Build the actual cross-compiled aspell
+    export LDFLAGS="$LDFLAGS -lintl"
+
+    make clean
+
+    ./configure --prefix=$PREFIX
+    make -j${CPU_COUNT}
+
+    make install
+
+fi
